5f4b48919c70bb7505ae7cba519ba461f2913052
Eldor Karimov <hacktivist19021@gmail.com>
2024-07-25 14:41:51 +0500

    added locale

--- news/api/serializers.py
+++ news/api/serializers.py
@@ -2,20 +2,32 @@ from rest_framework import serializers
 from news.models import News, NewsCategory, Tag
 
 class CategorySerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField()
     class Meta:
         model = NewsCategory
         fields = ('id', 'name', 'slug', 'created', 'updated')
     
+    def get_name(self, obj):
+        lang_code = self.context.get('lang_code', 'uz')
+        return getattr(obj, f"name_{lang_code}", obj.name)
+    
 class TagSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField()
     class Meta:
         model = Tag
         fields = ('id', 'name', 'slug', 'created', 'updated')
+
+    def get_name(self, obj):
+        lang_code = self.context.get('lang_code', 'uz')
+        return getattr(obj, f"name_{lang_code}", obj.name)
 
 
 class NewsSerializer(serializers.ModelSerializer):
     category = CategorySerializer()
     tags = TagSerializer(many = True)
     user = serializers.SerializerMethodField()
+    title = serializers.SerializerMethodField()
+    description = serializers.SerializerMethodField()
     
     class Meta:
         model = News
@@ -26,6 +38,14 @@ class NewsSerializer(serializers.ModelSerializer):
             'user':{'read_only': True},
             'tags':{'read_only': True},
         }
+    
+    def get_title(self, obj):
+        lang_code = self.context.get('lang_code')
+        return getattr(obj, f"title_{lang_code}", obj.title)
+
+    def get_description(self, obj):
+        lang_code = self.context.get('lang_code', 'uz')
+        return getattr(obj, f"description_{lang_code}", obj.description)
 
     def get_user(self, obj):
         return {
--- news/api/views.py
+++ news/api/views.py
@@ -6,10 +6,18 @@ from news.models import News, Tag, NewsCategory
 from .serializers import NewsSerializer, CategorySerializer, TagSerializer
 from django.shortcuts import get_object_or_404
 
+def get_lang_code(request):
+    lang_codes = ['uz', 'en', 'ru']
+    lang_code = request.GET.get('lang')
+    if lang_code not in lang_codes:
+        lang_code = 'uz'
+    return {"lang_code":lang_code}
+
+
 class NewsListAPIView(APIView):
     def get(self, request):
         news = News.objects.filter(is_published = True)
-        serializer = NewsSerializer(news, many = True)
+        serializer = NewsSerializer(news, many = True, context = get_lang_code(request))
         data = {
             'success': True,
             'data': serializer.data
@@ -19,7 +27,7 @@ class NewsListAPIView(APIView):
 class NewsDetailAPIView(APIView):
     def get(self, request, news_slug):
         new = get_object_or_404(News, slug = news_slug)
-        serializer = NewsSerializer(new)
+        serializer = NewsSerializer(new, context = get_lang_code(request))
         data = {
             'success': True,
             'data': serializer.data
@@ -33,7 +41,7 @@ class NewsByCategoryAPIView(APIView):
     def get(self, request, cat_slug):
         category = get_object_or_404(NewsCategory, slug = cat_slug)
         news = News.objects.filter(category = category).order_by('-created')
-        serializer = NewsSerializer(news, many = True)
+        serializer = NewsSerializer(news, many = True, context = get_lang_code(request))
         data = {
             'success': True,
             'news': serializer.data
@@ -47,7 +55,7 @@ class NewsByTagAPIView(APIView):
     def get(self, request, tag_slug):
         tag = get_object_or_404(Tag, slug = tag_slug)
         news = News.objects.filter(tags = tag).order_by('-created')
-        serializer = NewsSerializer(news, many = True)
+        serializer = NewsSerializer(news, many = True, context = get_lang_code(request))
         data = {
             'success': True,
             'news': serializer.data
