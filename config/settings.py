3a4fffec72e2ae26a82a8b2e9837a89899f75f48
Eldor Karimov <hacktivist19021@gmail.com>
2024-11-19 14:59:21 +0500

    some change

--- config/settings.py
+++ config/settings.py
@@ -79,11 +79,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000", 
-    "http://127.0.0.1:3000",
-    "https://iqtidor.samduufu.uz",
-]
+CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS', cast=Csv())
 
 CORS_ALLOW_METHODS = [
     "GET",
